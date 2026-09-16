a9830b7a48ec67a2b342402cb26961d8f50c75f6
Jonathan Bouyer <63496507+Jo-Byr@users.noreply.github.com>
2026-09-16 18:12:28 +0200

    fix: add web versioning (#14)

--- src/trame_router/module/vue3.py
+++ src/trame_router/module/vue3.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+from trame_router import __version__
 
 # Compute local path to serve
 serve_path = str(Path(__file__).with_name("vue3").resolve())
 
 # Serve directory for JS/CSS files
-serve = {"__trame_router3": serve_path}
+serve = {f"__trame_router3_{__version__}": serve_path}
 
 # List of JS files to load (usually from the serve path above)
-scripts = ["__trame_router3/trame-router.umd.js"]
+scripts = [f"__trame_router3_{__version__}/trame-router.umd.js"]
 
 # List of Vue plugins to install/load
 vue_use = ["trame_router"]
