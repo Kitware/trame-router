248dd8c06c29fdb6b2d610f052f6a5fca00d338d
Jo-Byr <jonathanbouyer24@gmail.com>
2026-09-16 14:16:26 +0200

    chore: add web versioning

--- src/trame_router/module/vue2.py
+++ src/trame_router/module/vue2.py
@@ -1,13 +1,14 @@
 from pathlib import Path
+from trame_router import __version__
 
 # Compute local path to serve
 serve_path = str(Path(__file__).with_name("vue2").resolve())
 
 # Serve directory for JS/CSS files
-serve = {"__trame_router2": serve_path}
+serve = {f"__trame_router2_{__version__}": serve_path}
 
 # List of JS files to load (usually from the serve path above)
-scripts = ["__trame_router2/trame-router.umd.min.js"]
+scripts = [f"__trame_router2_{__version__}/trame-router.umd.min.js"]
 
 # List of Vue plugins to install/load
 vue_use = ["trame_router"]
